04b0fbf67f9fcc0410310c92d11dfabb4c18384d
Rached SERGHINE <Rached.Serghine@dsi-ap.com>
2026-03-04 15:53:02 +0100

    04/03

---
+++ src/main/java/com/example/demo/controller/controller.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller; // <-- CETTE LIGNE EST CRUCIALE
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
+public class controller {
+    
+    @GetMapping("/api/hello")
+    public String sayHello() {
+        return "Bonjour depuis le Backend Java de Rached !"; // message affiché 
+    }
+}
